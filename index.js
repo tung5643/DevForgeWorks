63eb08e35a71db7c4ef4b8cf6d7de6accc5eea12
tung5643 <shandyshawpun0suy@hotmail.com>
2024-05-08 19:21:49 +0700

    First commit

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
-function countBits(num) {
-  const result = new Array(num + 1).fill(0);
-  for (let i = 1; i <= num; i++) {
-    result[i] = result[i >> 1] + (i & 1);
+function zigzagLevelOrder(root) {
+  if (!root) return [];
+  const result = [];
+  const queue = [root];
+  let zigzag = false;
+  while (queue.length) {
+    const levelSize = queue.length;
+    const currentLevel = [];
+    for (let i = 0; i < levelSize; i++) {
+      const node = queue.shift();
+      if (zigzag) currentLevel.unshift(node.val);
+      else currentLevel.push(node.val);
+      if (node.left) queue.push(node.left);
+      if (node.right) queue.push(node.right);
+    }
+    result.push(currentLevel);
+    zigzag = !zigzag;
   }
   return result;
 }
