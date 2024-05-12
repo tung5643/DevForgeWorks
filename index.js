317482174dedabdaf195036c6490bf076201a92b
tung5643 <shandyshawpun0suy@hotmail.com>
2024-05-12 18:56:01 +0700

    Initial commit

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
-function rotateArray(arr, n) {
-  return arr.slice(n).concat(arr.slice(0, n));
+function hasCycle(head) {
+  let slow = head;
+  let fast = head;
+  while (fast && fast.next) {
+    slow = slow.next;
+    fast = fast.next.next;
+    if (slow === fast) return true;
+  }
+  return false;
 }
