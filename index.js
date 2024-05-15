b5e2729040953e604d85816e30fc2242549b7acb
tung5643 <shandyshawpun0suy@hotmail.com>
2024-05-15 09:18:47 +0700

    update project commit

--- index.js
+++ index.js
@@ -1,24 +1,24 @@
-function exist(board, word) {
-  const rows = board.length;
-  const cols = board[0].length;
-  const dfs = (i, j, index) => {
-    if (index === word.length) return true;
-    if (i < 0 || i >= rows || j < 0 || j >= cols || board[i][j] !== word[index])
-      return false;
-    const temp = board[i][j];
-    board[i][j] = "";
-    const found =
-      dfs(i + 1, j, index + 1) ||
-      dfs(i - 1, j, index + 1) ||
-      dfs(i, j + 1, index + 1) ||
-      dfs(i, j - 1, index + 1);
-    board[i][j] = temp;
-    return found;
-  };
-  for (let i = 0; i < rows; i++) {
-    for (let j = 0; j < cols; j++) {
-      if (board[i][j] === word[0] && dfs(i, j, 0)) return true;
+function isMatch(s, p) {
+  const dp = Array.from(Array(s.length + 1), () =>
+    Array(p.length + 1).fill(false),
+  );
+  dp[0][0] = true;
+  for (let i = 1; i <= p.length; i++) {
+    if (p[i - 1] === "*") {
+      dp[0][i] = dp[0][i - 2];
     }
   }
-  return false;
+  for (let i = 1; i <= s.length; i++) {
+    for (let j = 1; j <= p.length; j++) {
+      if (s[i - 1] === p[j - 1] || p[j - 1] === ".") {
+        dp[i][j] = dp[i - 1][j - 1];
+      } else if (p[j - 1] === "*") {
+        dp[i][j] = dp[i][j - 2];
+        if (p[j - 2] === "." || s[i - 1] === p[j - 2]) {
+          dp[i][j] = dp[i][j] || dp[i - 1][j];
+        }
+      }
+    }
+  }
+  return dp[s.length][p.length];
 }
